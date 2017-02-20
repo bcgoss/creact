24ac7630f0128b7f8ec7666796931ec1d846705f
Bryan Goss <bcgoss@gmail.com>
2017-02-19 19:26:01 -0800

    Add filter class

---
+++ app/assets/javascripts/components/_select_filter.js.jsx
@@ -0,0 +1,17 @@
+var SelectFilter = React.createClass({
+  render (){
+    return(
+      <div>
+        <label>
+          Filter By
+        </label>
+        <select className="form-control">
+          <option value="all">All</option>
+          <option value="bad">Bad</option>
+          <option value="halfbad">Halfbad</option>
+          <option value="fantastic">Fantastic</option>
+        </select>
+      </div>
+    )
+  }
+}
